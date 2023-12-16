ec7a6b91bb90daaca75fc8d3357c79901d30b5f5
faanna <sakurayu96@gmail.com>
2023-12-16 22:32:41 +0900

    trip-js-ing

---
+++ script/_04_trip.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper(".mySwiper", {
+  slidePerView: 4,
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
